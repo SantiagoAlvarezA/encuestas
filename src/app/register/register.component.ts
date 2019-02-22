4d920403d60483d9b06802e09dc4cdeff05dc9da
Santiago Alvarez <aalvarez3570@gmail.com>
2019-02-21 19:13:37 -0500

    registro de estudiantes

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -9,11 +9,25 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  icon:string="user-graduate";
+  icon: string = "user-graduate";
   login: any = {};
   user: any = {};
 
+  rol: boolean = false;
   isAuthenticated: boolean = true;
+
+  group: Array<any> = [
+    '2019A', '2019B',
+    '2020A', '2020B',
+    '2021A', '2021B',
+    '2022A', '2022B',
+    '2023A', '2023B',
+    '2024A', '2024B',
+    '2025A', '2025B',
+    '2026A', '2026B',
+    '2027A', '2027B',
+    '2028A', '2028B'
+  ];
 
   constructor(private authentication: LoginService, private router: Router, private route: ActivatedRoute) {
     authentication.isAuthenticated().subscribe((result) => {
@@ -23,7 +37,8 @@ export class RegisterComponent implements OnInit {
       } else {
         this.isAuthenticated = false;
         this.user.rol = this.route.snapshot.params.rol;
-        this.icon = (this.user.rol == 'teacher')? 'chalkboard-teacher':'user-graduate';
+        this.icon = (this.user.rol == 'teacher') ? 'chalkboard-teacher' : 'user-graduate';
+        this.rol = (this.user.rol == 'teacher') ? false : true;
         // console.log(this.rol);
       }
     }, (error) => {
@@ -36,7 +51,9 @@ export class RegisterComponent implements OnInit {
   }
 
   public register() {
+    //pendiente para validaciones
     this.user.email = this.login.email;
     this.authentication.register(this.login.email, this.login.password, this.user);
+
   }
 }
