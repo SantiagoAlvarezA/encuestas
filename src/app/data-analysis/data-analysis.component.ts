63d92b9a14aad7d8ee5f69951107027d7a7300b5
Santiago Alvarez <aalvarez3570@gmail.com>
2019-02-24 22:57:04 -0500

    data by grpup

--- src/app/data-analysis/data-analysis.component.ts
+++ src/app/data-analysis/data-analysis.component.ts
@@ -26,6 +26,8 @@ export class DataAnalysisComponent implements OnInit {
   correctas: Array<any> = [];
   incorrectas: Array<any> = [];
   preguntas: any[] = [];
+  selectedGroup: string = '';
+  selectedProgram: string = '';
 
   constructor(private authentication: LoginService, private router: Router, private db: AngularFireDatabase, private testService: TestService) {
     this.test = this.testService.data;
@@ -48,10 +50,35 @@ export class DataAnalysisComponent implements OnInit {
   ngOnInit() {
 
   }
+  imprimeGroup() {
+    console.log(this.selectedGroup, ' gruuporrrrrr');
+
+  }
 
   dataByGroups() {
     this.byTest = false;
     this.byGroup = !this.byGroup;
+    var db = this.db.database;
+    let students = db.ref('users').orderByChild('group').equalTo(this.selectedGroup);
+    students.on('child_added', students => {
+      console.log(students.child('program'));
+      if (students.val().program == this.selectedProgram)
+        console.log(students.val(), ' estudiantes');
+    });
+
+    db.ref('typeQuestion').orderByChild('testId').equalTo(this.test.id).on('child_added', theme => {
+      db.ref('question').orderByChild('themeId').equalTo(theme.val().id).on('child_added', question => {
+        db.ref('results').orderByChild('questionId').equalTo(question.val().id).once('value', snapshot => {
+
+          // console.log(theme.val());
+          // console.log(question.val());
+          // console.log(snapshot.val());
+
+
+        });
+      });
+    });
+
   }
 
 
@@ -91,33 +118,33 @@ export class DataAnalysisComponent implements OnInit {
 
 
 
-  public barChartOptions: any = {
-    scaleShowVerticalLines: true,
-    responsive: true
-  };
+  // public barChartOptions: any = {
+  //   scaleShowVerticalLines: true,
+  //   responsive: true
+  // };
 
 
 
 
 
-  public barChartLabels: string[] = this.preguntas;
-  public barChartType: string = 'bar';
-  public barChartLegend: boolean = true;
+  // public barChartLabels: string[] = this.preguntas;
+  // public barChartType: string = 'bar';
+  // public barChartLegend: boolean = true;
 
-  public barChartData: any[] = [
-    { data: [2,3,0], label: 'correctas' },
-    { data: [4,6,0], label: 'incorrectas' },
-  ];
+  // public barChartData: any[] = [
+  //   { data: [2,3,0], label: 'correctas' },
+  //   { data: [4,6,0], label: 'incorrectas' },
+  // ];
 
 
-  // events
-  public chartClicked(e: any): void {
-    console.log(e, 'eeeeeeeeeeeee');
-  }
+  // // events
+  // public chartClicked(e: any): void {
+  //   console.log(e, 'eeeeeeeeeeeee');
+  // }
 
-  public chartHovered(e: any): void {
-    console.log(e);
-  }
+  // public chartHovered(e: any): void {
+  //   console.log(e);
+  // }
 
 
 }
