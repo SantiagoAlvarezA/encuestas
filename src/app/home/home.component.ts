a1871704ac1696d4e6ab0928f7ccd450e7d4f317
Santiago Alvarez <aalvarez3570@gmail.com>
2019-03-08 15:20:19 -0500

    imagenes en home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
   isAuthenticated: boolean = false;
   showNavigationArrows = false;
   showNavigationIndicators = false;
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+  images = [1, 2, 3].map(() => `https://firebasestorage.googleapis.com/v0/b/algoritbuild-10.appspot.com/o/50469991_242412170002805_3679487508841758720_n.jpg?alt=media&token=22c1f366-3dc1-4c96-a165-91c6a19ba2f2`);
 
   constructor(config: NgbCarouselConfig, private _http: HttpClient,
       private authentication: LoginService) {
